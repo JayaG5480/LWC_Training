4aefbbae54317e790059768bcc16c22e6f49fd6f
jaya <gandla.jaya5480@gmail.com>
2024-08-21 14:20:13 +0530

    Add LWC_Training file

--- Destructuring/script.js
+++ Destructuring/script.js
@@ -3,6 +3,7 @@ let Myfruits={
     firstfruit:"Apple",
     secondfruit: "Banana",
     thirdfruit:"Mango",
+    forthfruit:"Cherry",
 };
 console.log("Myfruits",Myfruits);
 /*Array Destructuring
@@ -11,7 +12,8 @@ console.log("f1",f1);
 console.log("f2",f2);
 console.log(f3);*/
 //object destructuring
-let {firstfruit,secondfruit,thirdfruit}=Myfruits;
+let {firstfruit,secondfruit,thirdfruit,forthfruit}=Myfruits;
 console.log(firstfruit);
 console.log(secondfruit);
 console.log(thirdfruit);
+console.log(forthfruit);
